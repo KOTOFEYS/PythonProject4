a7e4bbb220531bbd0005ee128aa0227c155aca63
KOTOFEYS <makarov.k@mail.ru>
2024-07-27 01:08:09 +0300

    "Произвольное число параметров".

---
+++ modul3.4.py
@@ -0,0 +1,13 @@
+def single_root_words(root_word, *other_words):
+    some_words = []
+    for i in other_words:
+        if root_word in i:
+            some_words.append(i)
+            continue
+        else:
+            continue
+        return some_words
+    print(some_words)
+single_root_words('rich', 'rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+single_root_words('able', 'Disablement'.lower(), 'Able'.lower(), 'Mable'.lower(),
+                  'Disable'.lower(), 'Bagel'.lower())
